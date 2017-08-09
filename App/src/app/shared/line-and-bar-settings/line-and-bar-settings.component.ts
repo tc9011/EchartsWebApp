90502a25ecc127383eb0e2562031a04dc8745f87
tc9011 <tc9011@126.com>
2017-08-09 22:19:53 +0800

    feat(setting):add input for title

--- App/src/app/shared/line-and-bar-settings/line-and-bar-settings.component.ts
+++ App/src/app/shared/line-and-bar-settings/line-and-bar-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-line-and-bar-settings',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineAndBarSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translate: TranslateService) { }
 
   ngOnInit() {
   }
