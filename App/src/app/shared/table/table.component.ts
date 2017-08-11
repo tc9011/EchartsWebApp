74d7030ae6ab0ab2af7dcd083dc2062a63d2b4fa
tangcheng6092002453 <tang.cheng2@zte.com.cn>
2017-08-11 12:01:19 +0800

    refactor(table):delete unused import

--- App/src/app/shared/table/table.component.ts
+++ App/src/app/shared/table/table.component.ts
@@ -1,14 +1,5 @@
 import {Component, EventEmitter, Output, OnChanges} from '@angular/core';
-import {DataSource} from '@angular/cdk';
 
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/observable/fromEvent';
 import {TranslateService} from "@ngx-translate/core";
 import {SettingsService} from "../servers/settings.service";
 
