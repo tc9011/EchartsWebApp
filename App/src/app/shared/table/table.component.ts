e97f073d2f41129a1f9421b8b28fd48c45e3752e
tangcheng6092002453 <tang.cheng2@zte.com.cn>
2017-08-11 15:26:40 +0800

    feat(table):add delete button

--- App/src/app/shared/table/table.component.ts
+++ App/src/app/shared/table/table.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output, OnChanges} from '@angular/core';
+import {Component, EventEmitter, Input, Output, OnInit, AfterViewInit, ElementRef, ViewChild} from '@angular/core';
 
 import {TranslateService} from "@ngx-translate/core";
 import {SettingsService} from "../servers/settings.service";
@@ -8,12 +8,13 @@ import {SettingsService} from "../servers/settings.service";
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnChanges {
+export class TableComponent implements OnInit {
+  @ViewChild('deleteBtn') deleteBtn:any;
   @Output() dataChange:EventEmitter<any> = new EventEmitter();
   data:any;
   numberOfDataGroup:Array<number>;
 
-  constructor(public translate: TranslateService, public settingsService:SettingsService){
+  constructor(public translate: TranslateService, public settingsService:SettingsService, private el:ElementRef){
     this.numberOfDataGroup = [1];
     this.data = {
       "xCoordinates": "",
@@ -24,8 +25,8 @@ export class TableComponent implements OnChanges {
   }
 
 
-  ngOnChanges(){
-
+  ngOnInit(){
+    this.deleteBtn.disabled = true;
   }
 
   save(){
@@ -36,11 +37,15 @@ export class TableComponent implements OnChanges {
 
   addItem(){
     this.numberOfDataGroup.push(this.numberOfDataGroup[this.numberOfDataGroup.length - 1] + 1);
+    if (this.numberOfDataGroup.length > 1){
+      this.deleteBtn.disabled = false;
+    }
   }
 
   deleteItem(){
-    if (this.numberOfDataGroup.length > 1){
-      this.numberOfDataGroup.pop();
+    this.numberOfDataGroup.pop();
+    if (this.numberOfDataGroup.length === 1){
+      this.deleteBtn.disabled = true;
     }
   }
 
