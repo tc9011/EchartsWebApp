e59dad1a359158050f6be7bf19f8b9f29ef45dba
tc9011 <tc9011@126.com>
2017-08-12 19:19:59 +0800

    feat(line):add data button works

--- App/src/app/line-charts/line-charts.component.ts
+++ App/src/app/line-charts/line-charts.component.ts
@@ -10,7 +10,7 @@ import {TranslateService} from "@ngx-translate/core";
 })
 export class LineChartsComponent implements OnInit {
   @ViewChild('deleteBtn') deleteBtn:any;
-  numberOfDataGroup:Array<number>;
+  dataGroup:Array<any>;
   echartsIntance:any;
   chartOption:any;
 
@@ -19,7 +19,11 @@ export class LineChartsComponent implements OnInit {
 
   constructor(private settingsService: SettingsService, public translate: TranslateService) {
     this.chartOption = this.settingsService.exampleSettings;
-    this.numberOfDataGroup = [1];
+    this.dataGroup = [{
+      title: '邮件营销',
+      xData: '周一,周二,周三,周四,周五,周六,周日',
+      yData: '120, 132, 101, 134, 90, 230, 210'
+    }];
     this.settings = {
       'title': '',
       'subtitle': '',
@@ -34,8 +38,9 @@ export class LineChartsComponent implements OnInit {
     this.deleteBtn.disabled = true;
   }
 
+  // 重新加载echarts
   reloadEcharts(){
-    this.chartOption = this.settingsService.setChartOption(this.settings);
+    this.chartOption = this.settingsService.setChartOption(this.settings,this.dataGroup);
     this.echartsIntance.setOption(this.chartOption);
   }
 
@@ -43,18 +48,37 @@ export class LineChartsComponent implements OnInit {
     this.echartsIntance = ec;
   }
 
+  // 增加一块输入区域
   addItem(){
-    this.numberOfDataGroup.push(this.numberOfDataGroup[this.numberOfDataGroup.length - 1] + 1);
-    if (this.numberOfDataGroup.length > 1){
+    let _tempObject = {
+      title: '',
+      xData: '',
+      yData: ''
+    };
+    this.dataGroup.push(_tempObject);
+
+    //有多个输入区块，删除按钮可用
+    if (this.dataGroup.length > 1){
       this.deleteBtn.disabled = false;
     }
   }
 
+  // 删除一块输入区域
   deleteItem(){
-    this.numberOfDataGroup.pop();
-    if (this.numberOfDataGroup.length === 1){
+    //删除配置中相应的数据
+    this.dataGroup.pop();
+
+    this.reloadEcharts();
+
+    //只剩一块输入区时，删除按钮不可用
+    if (this.dataGroup.length === 1){
       this.deleteBtn.disabled = true;
     }
+  }
+
+  clearData(){
+    this.dataGroup = [];
+    this.reloadEcharts();
   }
 
 }
--- App/src/app/shared/servers/settings.service.ts
+++ App/src/app/shared/servers/settings.service.ts
@@ -146,70 +146,67 @@ export class SettingsService {
           type : 'value',
         }
       ],
-      series : [
-        {
-          name:'邮件营销',
-          type:'line',
-          stack: '总量',
-          areaStyle: {normal: {}},
-          data:[120, 132, 101, 134, 90, 230, 210]
-        },
-        {
-          name:'联盟广告',
-          type:'line',
-          stack: '总量',
-          areaStyle: {normal: {}},
-          data:[220, 182, 191, 234, 290, 330, 310]
-        },
-        {
-          name:'视频广告',
-          type:'line',
-          stack: '总量',
-          areaStyle: {normal: {}},
-          data:[150, 232, 201, 154, 190, 330, 410]
-        },
-        {
-          name:'直接访问',
-          type:'line',
-          stack: '总量',
-          areaStyle: {normal: {}},
-          data:[320, 332, 301, 334, 390, 330, 320]
-        },
-        {
-          name:'搜索引擎',
-          type:'line',
-          stack: '总量',
-          label: {
-            normal: {
-              show: true,
-              position: 'top'
-            }
-          },
-          areaStyle: {normal: {}},
-          data:[820, 932, 901, 934, 1290, 1330, 1320]
-        }
-      ]
+      series : []
     };
   }
 
-  setChartOption(settings){
+  // 配置chartoption
+  setChartOption(settings:any,dataGroup:Array<any>){
     this.option.title.text = settings.title?settings.title:'堆叠区域图';
     this.option.title.subtext = settings.subtitle?settings.subtitle:'power by echarts';
     this.option.title.textStyle.color = settings.titleColor?settings.titleColor:"black";
     this.option.title.subtextStyle.color = settings.subtitleColor?settings.subtitleColor:"balck";
-    this.option.xAxis.data = this.splitData(settings.xData)?this.splitData(settings.xData):['周一','周二','周三','周四','周五','周六','周日'];
-    this.option.series[0].data = this.splitData(settings.yData)?this.splitData(settings.yData):[120, 132, 101, 134, 90, 230, 210];
+
+    this.setXYData(dataGroup);
 
     return this.option;
   }
 
+  // 设置xy轴数据
+  setXYData(dataGroup){
+    let dataLength = dataGroup.length,
+        seriesLength = this.option.series.length;
 
+    if (dataLength < seriesLength){                     //删除已经删除的数据数据
+      for (let m = 0; m < seriesLength-dataLength; m++){
+        this.option.series.pop();
+      }
+    }else {
+      for (let i = 0; i < dataLength; i++){
+        for (let j = 0; j <= seriesLength; j++){
+          if (this.option.series[i]){                     //当option.series中包含dataGroup的数据
+            this.option.series[i].name = dataGroup[i].title;
+            this.option.series[i].data = this.splitData(dataGroup[i].yData);
+            this.option.xAxis[0].data = this.splitData(dataGroup[i].xData);
+          } else {                                        //当option.series中不包含dataGroup的数据
+            let yData = {
+              name: dataGroup[i].title,
+              type:'line',
+              data:this.splitData(dataGroup[i].yData)
+            };
+            this.option.series.push(yData);
+            this.option.xAxis[0].data = this.splitData(dataGroup[i].xData);
+          }
+        }
+      }
+    }
+  }
+
+
+  // 处理用户输入数据中的空格、中英文逗号
   splitData(data:any){
     let dataArray = [];
 
     if(data){
       data = String(data).replace(/\s|\,|\，/g, ' ');
       dataArray = data.split(" ");
+
+      //去除空格影响
+      for (let i = 0; i < dataArray.length; i++){
+        if (!dataArray[i]){
+          dataArray.splice(i,1);
+        }
+      }
     }
 
     return dataArray;
