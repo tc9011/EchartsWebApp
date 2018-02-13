4663f3d82f120bd5c135422ef4387a018b9a554e
10227461 <tang.cheng11@zte.com.cn>
2018-02-13 17:17:09 +0800

    refactor(all):change route

--- App/src/app/app.routes.ts
+++ App/src/app/app.routes.ts
@@ -2,10 +2,6 @@ import {AppComponent} from "./app.component";
 
 export const appRoutes = [
   {
-    path: '',
-    loadChildren: './home/home.module#HomeModule',    // lazy load
-  },
-  {
     path: 'line',
     loadChildren: './line-charts/line-charts.module#LineChartsModule',    // lazy load
   },
