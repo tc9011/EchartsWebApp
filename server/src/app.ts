813b1b9f165af871fee4e5e9fdc4c8c5d723e4c1
tc9011 <tc9011@126.com>
2018-08-08 23:26:37 +0800

    feat(line):itemGap can be changed

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,8 @@
 import * as Koa from 'koa';
-import * as bodyParser from 'koa-bodyparser'
-import * as puppeteer from 'puppeteer';
-import * as fs from 'fs';
 const app = new Koa;
 
-app.use(async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto('https://www.jd.com');
-    await page.setViewport({
-        width: 1200,
-        height: 800
-    });
-    await page.screenshot({path: 'jd.png'});
-
-    await browser.close();
+app.use(async ctx => {
+    ctx.body = 'hello world';
 });
 
 app.listen(3000);
-
-export default app;
