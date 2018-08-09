74c3e68f519a179ef056ee271863e8fa7e509cb3
tc9011 <tc9011@126.com>
2018-08-09 23:19:37 +0800

    refactor(server):add crawler

--- server/src/controllers/crawler.js
+++ server/src/controllers/crawler.js
@@ -1,12 +1,54 @@
 import { configs } from '../config/config';
 import * as cheerio from 'cheerio';
 
+/*
+* *******************************************************
+ [{
+   name: '',
+   subOptions: {
+     name: '',
+     type: '',
+   },
+ }]
+* *******************************************************
+* */
+let options = [];
+
 class Crawler {
   constructor() {
 
   }
 
+  filterChapters(html) {
+    const $ = cheerio.load(html);
 
+    const list = $('.dtui-treelist-parent .ecdoc-api-tree-text-prop').text();
+
+    $chapters.each(function (item) {
+      var $chapter = $(this);
+      var chapterTitle = $chapter.find('h3').text().trim();
+      var videos = $chapter.find('.video').children('li');
+      var chapterData = {
+        chapterTitle: chapterTitle,
+        videos: [],
+      };
+
+      videos.each(function (item) {
+        var $video = $(this).find('.J-media-item');
+        var videoTitle = $video.text().split('(')[0].trim();
+        var id = $video.attr('href').split('video/')[1];
+
+        chapterData.videos.push({
+          title: videoTitle,
+          id: id,
+        });
+      });
+
+      courseData.videos.push(chapterData);
+    });
+
+    return courseData;
+  }
 }
 
 export { Crawler };
