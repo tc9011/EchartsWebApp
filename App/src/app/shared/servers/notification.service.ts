a9b373b262d521a7076c4fced33390ba972f7c23
10227461 <tang.cheng11@zte.com.cn>
2018-08-10 15:27:10 +0800

    feat(server): add server

--- App/src/app/shared/servers/notification.service.ts
+++ App/src/app/shared/servers/notification.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {Subject} from 'rxjs/Subject';
+import {Subject} from 'rxjs';
 import {NotificationTypings} from '../typings/notification.typings';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class NotificationService {
