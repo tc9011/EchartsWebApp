8f847b36658580ca5376c4bac982b0d0c0213752
tc9011 <tc9011@126.com>
2018-10-26 00:03:21 +0800

    refactor(layout): move home component to layout module

--- App/src/app/layout/layout.module.ts
+++ App/src/app/layout/layout.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { GlobalComponent } from './global/global.component';
 import { SharedModule } from '../shared/shared.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
 const LAYOUTCOMPONENT = [
   GlobalComponent,
-  SidebarComponent
+  SidebarComponent,
+  HomeComponent
 ];
 
 @NgModule({
